66a6c239560db9a9d8f2885f3fee6ec0da6fc74c
Gezuiz <69606836+Gezuiz@users.noreply.github.com>
2020-11-12 18:26:08 -0300

    Add files via upload

---
+++ SCRIPTS/ControleGameNetwork.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ControleGameNetwork : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("GameRoom", new RoomOptions { MaxPlayers = 4 },TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("prefabPlayer", new Vector3(-1,1,1), Quaternion.identity);
+        PhotonNetwork.Instantiate("prefabCamera", new Vector3(-2, 8, -7), Quaternion.Euler(45, 0, 0));
+    }
+}
